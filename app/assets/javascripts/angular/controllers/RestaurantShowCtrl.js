45182d784612bb68a538f083ac6985334539117d
bhaveshletter <bhaveshletter@gmail.com>
2014-11-14 15:03:20 +0530

    Added new empty file for apis

--- app/assets/javascripts/angular/controllers/RestaurantShowCtrl.js
+++ app/assets/javascripts/angular/controllers/RestaurantShowCtrl.js
@@ -1,4 +1,5 @@
 restaurants.controller('RestaurantShowCtrl', ['$scope', '$http', '$routeParams', function(scope, http, routeParams){
+	
 	scope.restaurant = []
 	http.get('./restaurants/' + routeParams.id + '.json').success(function(data){
 		scope.restaurant = data
